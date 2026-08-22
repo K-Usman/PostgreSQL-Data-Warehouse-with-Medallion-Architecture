d4f3d33017e1896fd1646b51dd2aed41b00bd51e
Mohammad Usman Kamran <60486077+K-Usman@users.noreply.github.com>
2026-08-22 16:06:28 +0200

    Refactor load_bronze procedure with schema prefix
    Updated stored procedure to include schema prefix and corrected usage instructions.

--- scripts/load/bronze.sql
+++ scripts/load/bronze.sql
@@ -3,15 +3,15 @@
 STORED PROCEDURE
 ==========================================================================================================
 Purpose: This script creates a stored procure load_bronze for loading raw source data from csv to our tables in bronze schema.
-Use: CALL load_bronze();
+Use: CALL bronze.load_bronze();
 NOTES:
 	- COPY command runs on the server side.Therefore csv files must be accessible to Postgres server and has to be placed in the directory.
 	- I had to write table attribute names in each insert. Because I am adding a metadata column _load_date to every table and COPY command maps 
    CSV columns by positional order to every column in the table.
 */
 
-DROP PROCEDURE IF EXISTS load_bronze();
-CREATE PROCEDURE load_bronze()
+DROP PROCEDURE IF EXISTS bronze.load_bronze;
+CREATE PROCEDURE bronze.load_bronze()
 LANGUAGE plpgsql
 AS $$
 DECLARE
@@ -151,5 +151,3 @@ BEGIN
 	RAISE NOTICE 'Raw data loaded in %',(v_start_time-v_end_time);
 END;
 $$;
-
-
